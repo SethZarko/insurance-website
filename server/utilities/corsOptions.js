5802fe0fc97ddd04aa5ed7c6e3e06f1661f531d9
SethZarko <sethzarkovich@gmail.com>
2024-08-05 20:30:26 -0400

    Add CORS

--- server/utilities/corsOptions.js
+++ server/utilities/corsOptions.js
@@ -1,7 +1,8 @@
 export const corsOptions = {
     origin: [
         'http://localhost:3000',
-        'https://www.getpostman.com'
+        'https://www.getpostman.com',
+        'https://insurance-website-client.onrender.com'
     
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
